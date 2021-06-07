8589be8ba7f22f4a96c7ddcd224af5fefa284e32
htauL <80499850+htauL@users.noreply.github.com>
2021-06-07 20:08:21 -0300

    Add files via upload

---
+++ text rpg/items.py
@@ -0,0 +1,34 @@
+#----------consumables-----------
+potion = {
+    'NAME': 'Lesser Healing Potion',
+    'BPRICE': 20,
+    'SPRICE': 10,
+    'DESC': 'Heals 25 HP.'
+}
+
+#---------equipment--------------
+
+#--swords--
+wooden_sword = {
+    'NAME': 'Wooden Sword',
+    'BPRICE': 20,
+    'SPRICE': 10,
+    'DESC': 'Deals +5 damage.'
+}
+
+copper_sword = {
+    'NAME': 'Copper Sword',
+    'BPRICE': 50,
+    'SPRICE': 25,
+    'DESC': 'Deals +15 damage.'
+}
+
+#--breastplates--
+
+lather_breastplate = {
+    'NAME': 'Leather Breastplate',
+    'BPRICE': 75,
+    'SPRICE': 35,
+    'DESC': 'Gives +10 defense.'
+}
+
--- text rpg/main.py
+++ text rpg/main.py
@@ -1,8 +1,11 @@
 import math
 import time
 
+import items 
 import enemies
 import players
+
+i = items
 
 print('Starting adventure... \n')
 
@@ -10,10 +13,10 @@ print('Starting adventure... \n')
 def EnterCombat(player, enemy):
     print(f'An enemy', enemy['NAME'], 'appeared!')
     time.sleep(.7)
-    while enemy['HP'] > 0 or player['HP'] > 0:
+    while True:
         print('What to do?')
-        choice = input('[R]un [F]ight ').upper().strip()
-        if choice == 'F' or choice == 'FIGHT':
+        choice = input('[R]un\n[F]ight\n\n-').upper().strip()
+        if choice == 'F':
             time.sleep(.8)
             print('You chose to fight \n')         
             if player['SPE'] > enemy['SPE']:
@@ -42,13 +45,14 @@ def EnterCombat(player, enemy):
             if enemy['HP'] <= 0 or player['HP'] <= 0:
                 enemy['HP'] = enemy['MAXHP']  
                 break
-        elif choice == 'R' or choice == 'RUN':
+        elif choice == 'R':
             print('You have escaped! \n')
             enemy['HP'] = enemy['MAXHP']
             break
 
         else:
-            print('Choose a valid option! \n')
+            print('Choose a valid option!\n')
+            time.sleep(.8)
 
 #----------level up system--------------
 def LevelUp(player, level):
@@ -64,35 +68,66 @@ def LvlCheck(player):
 
 #-------------store------------------
 def EnterStore(player):
-	print('You entered the store \n')
-	time.sleep(.8)
-	print('Merchant: Greetings. How can I help you today?')
-	while True:
-		time.sleep(1)
-		choice = input('[B]uy [S]ell [L]eave ').upper().strip()
-		if choice == 'L' or choice == 'LEAVE':
-			time.sleep(.8)
-			print('Merchant: See you next time.\n')
-			break
+    print('You entered the store \n')
+    time.sleep(.8)
+    print('Merchant: Greetings. How can I help you today?\n')
+    while True:
+        time.sleep(1)
+        choiceB = input('[B]uy\n[S]ell\n[L]eave\n\n-').upper().strip()
+        if choiceB == 'L':
+            time.sleep(.8)
+            print('Merchant: See you next time.\n')
+            break
 			
-		if choice == 'B' or choice == 'BUY':
-			time.sleep(.8)
-			print('Which one of those items do you want to buy?\n')
-			while True:
-				time.sleep(.8)
-				print('[1]Potion(20G)\n[2]Copper Sword\n[3]Leather Breastplate\n\n[0]Exit \n')
-				time.sleep(.1)
-				item = input('select the item ')
-				if item == '0':
-					time.sleep(.8)
-					print('Merchant: Anything else?\n')
-					break
-				else:
-					time.sleep(.8)
-					print('Choose a valid option! \n')
+        elif choiceB == 'B':
+            time.sleep(.8)
+            print('Which one of those items do you want to buy?\n')
+            while True:
+                time.sleep(.8)
+                print('*You have:', player['G'],'G\n')
+                print('[1]', i.potion['NAME'], i.potion['BPRICE'], 'G      --', i.potion['DESC'])
+                print('[2]', i.copper_sword['NAME'], i.copper_sword['BPRICE'], 'G      --', i.copper_sword['DESC'])
+                print('[3]', i.lather_breastplate['NAME'], i.lather_breastplate['BPRICE'], 'G      --', i.lather_breastplate['DESC'])
+                print('\n[0] Exit \n')
+                time.sleep(.1)
+                item = input('select the item ')
+                if item == '0':
+                    time.sleep(.8)
+                    print('Merchant: Anything else?\n')
+                    break
+                elif item == '1':
+                    if player['G'] >= i.potion['BPRICE']:
+                        time.sleep(.8)
+                        player['G'] -= i.potion['BPRICE']
+                        print('Merchant: There is.')
+                        time.sleep(.8)
+                        print('Merchant: Anything else?\n')
+                    else:
+                        print('Merchant: You do not have enough money.\n')
+                elif item == '2':
+                    if player['G'] >= i.copper_sword['BPRICE']:
+                        time.sleep(.8)
+                        player['G'] -= i.copper_sword['BPRICE']
+                        print('Merchant: There is.')
+                        time.sleep(.8)
+                        print('Merchant: Anything else?\n')
+                    else:
+                        print('Merchant: You do not have enough money.\n')
+                elif item == '3':
+                    if player['G'] >= i.lather_breastplate['BPRICE']:
+                        time.sleep(.8)
+                        player['G'] -= i.lather_breastplate['BPRICE']
+                        print('Merchant: There is.')
+                        time.sleep(.8)
+                        print('Merchant: Anything else?\n')
+                    else:
+                        print('Merchant: You do not have enough money.\n')
+                else:
+                    time.sleep(.8)
+                    print('Choose a valid option! \n')
 				
-		else:
-			print('Choose a valid option!\n')
+        else:
+            print('Choose a valid option!\n')
 
 
 while True:
@@ -100,15 +135,15 @@ while True:
 	if players.player['HP'] > 0:
 		print('You are in the city. What to do?')
 		time.sleep(.5)
-		choiceA = input('[A]dventure [S]tore ').upper().strip()
-		if choiceA == 'A' or choiceA == 'ADVENTURE':
+		choiceA = input('[A]dventure\n[S]tore\n\n-').upper().strip()
+		if choiceA == 'A':
 			time.sleep(.8)
 			print('You went on an adventure! \n')
 			EnterCombat(players.player, enemies.slime)
-		if choiceA == 'S' or choiceA == 'STORE':
+		elif choiceA == 'S':
 			EnterStore(players.player)
 		else:
-			print('Choose a valid option! \n')
+			print('Choose a valid option!\n')
 	if players.player['HP'] <= 0:
 		print('Game over')
 		break
